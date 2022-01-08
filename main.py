27bdf81104947b6fddfc3da02c62286b3c399625
Pedro Alexandre Wollinger <pwollinger02@gmail.com>
2022-01-08 17:00:05 -0300

    Fixed the finalImage bug. Changed the file format to jpeg. Chaged the finalImage name to generate_final_cover

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import re
-import PIL
 import time
 import spotipy
 import requests
 import configparser
-from PIL import ImageFilter, Image
+from album_cover_generator import finalImage
 from spotipy.oauth2 import SpotifyOAuth
 
 config = configparser.ConfigParser()
@@ -38,7 +37,8 @@ while True:
                     print(f"Listening: {artist_name} - {track_name}")
 
             response = requests.get(current_track['item']['album']['images'][0]['url'])
-            album_blur = finalImage("album.png")
+            time.sleep(1)
+            album_blur = generate_final_cover(response.content, "album.jpeg")
 
     except TypeError:
         with open("playing.txt", "w", encoding='utf8') as song:
@@ -46,21 +46,3 @@ while True:
 
     song_duration = (current_track['item']['duration_ms'] - current_track['progress_ms'])/1000
     time.sleep(song_duration)
-
-def finalImage(coverFilename):
-    with Image.open(coverFilename) as cover:
-        newSize = (int(round(cover.size[0]/cover.size[1]*1080)), 1080)
-        coverResized = cover.resize(newSize)
-        blurredCover = coverResized.filter(ImageFilter.GaussianBlur(6))
-
-        remainder = 1920 - newSize[0]
-        cutpoints = (remainder//2,
-                    (1920 - remainder//2, blurredCover.size[0] - remainder%2 - remainder//2))
-        strp1 = blurredCover.crop((0, 0, cutpoints[0], 1080))
-        strp2 = blurredCover.crop((cutpoints[1][1], 0, blurredCover.size[0], 1080))
-
-        finalCover = Image.new(cover.mode, (1920, 1080))
-        finalCover.paste(strp1, (0,0))
-        finalCover.paste(coverResized, (cutpoints[0],0))
-        finalCover.paste(strp2, (cutpoints[1][0],0))
-        finalCover.save('album_blur.png')
