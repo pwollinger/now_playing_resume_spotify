0782e36ff4d3eca1912352e87f2c4b68f47336ca
Pedro Alexandre Wollinger <pwollinger02@gmail.com>
2022-01-08 17:21:34 -0300

    Changed album_cover_generator.py variables names²

--- album_cover_generator.py
+++ album_cover_generator.py
@@ -6,17 +6,17 @@ def generate_final_cover(cover, coverFilename):
 
     cover = Image.open(io.BytesIO(cover))
 
-    newSize = (int(round(cover.size[0]/cover.size[1]*1080)), 1080)
-    coverResized = cover.resize(newSize)
-    blurredCover = coverResized.filter(ImageFilter.GaussianBlur(6))
+    new_size = (int(round(cover.size[0]/cover.size[1]*1080)), 1080)
+    cover_resized = cover.resize(new_size)
+    blurred_cover = cover_resized.filter(ImageFilter.GaussianBlur(6))
 
-    remainder = 1920 - newSize[0]
-    cutpoints = (remainder//2, (1920 - remainder//2, blurredCover.size[0] - remainder%2 - remainder//2))
-    strp1 = blurredCover.crop((0, 0, cutpoints[0], 1080))
-    strp2 = blurredCover.crop((cutpoints[1][1], 0, blurredCover.size[0], 1080))
+    remainder = 1920 - new_size[0]
+    cutpoints = (remainder//2, (1920 - remainder//2, blurred_cover.size[0] - remainder%2 - remainder//2))
+    strp1 = blurred_cover.crop((0, 0, cutpoints[0], 1080))
+    strp2 = blurred_cover.crop((cutpoints[1][1], 0, blurred_cover.size[0], 1080))
 
-    finalCover = Image.new(cover.mode, (1920, 1080))
-    finalCover.paste(strp1, (0,0))
-    finalCover.paste(coverResized, (cutpoints[0],0))
-    finalCover.paste(strp2, (cutpoints[1][0],0))
-    finalCover.save(coverFilename)
+    final_cover = Image.new(cover.mode, (1920, 1080))
+    final_cover.paste(strp1, (0,0))
+    final_cover.paste(cover_resized, (cutpoints[0],0))
+    final_cover.paste(strp2, (cutpoints[1][0],0))
+    final_cover.save(coverFilename)
